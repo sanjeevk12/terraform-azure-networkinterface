fbe57b6169bad0b22b4c3ddf6236dcdbe38644e7
sanjeevk12 <55358122+sanjeevk12@users.noreply.github.com>
2020-01-30 16:47:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.nic_name}"
+  location            = "${var.location}"
+  resource_group_name = "${var.nic_rg_name}"
+
+  ip_configuration {
+    name                          = "${var.nic_ip_config}"
+    subnet_id                     = "${data.azurerm_subnet.sub.id}"
+    private_ip_address_allocation = "${var.private_ip_address_allocation_type}"
+    private_ip_address            = "${var.private_ip_address}"
+  }
+
+  tags = "${var.tags}"
+}
